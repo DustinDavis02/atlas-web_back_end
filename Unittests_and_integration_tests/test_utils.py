8707e006854a9cf02cd108d8067b6b7c4703b8d9
Dustin Davis <davisdustin328@gmail.com>
2024-01-31 13:02:08 -0600

    Parameterize and patch

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,9 @@
 """Unittests for utils module."""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """Testing function access_nested_map."""
@@ -22,7 +23,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), "b")
     ])
     def test_access_nested_map_exception(self,
-                                         nested_map, path, expected_message):
+        nested_map, path, expected_message):
         """Testing the function raises KeyError for specific inputs."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
@@ -42,3 +43,25 @@ class TestGetJson(unittest.TestCase):
             mock_requests_get.return_value = Mock(json=lambda: test_payload)
             self.assertEqual(get_json(test_url), test_payload)
             mock_requests_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Testing memoize decorator."""
+
+    def test_memoize(self):
+        """Testing memoize caches the result of a method."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mocked_method:
+            test_instance = TestClass()
+            first_result = test_instance.a_property
+            second_result = test_instance.a_property
+            self.assertEqual(first_result, 42)
+            self.assertEqual(second_result, 42)
+            mocked_method.assert_called_once()
