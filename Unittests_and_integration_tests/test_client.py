1b5e0d0cd979c0c70dd221701ab4a105543d0391
Dustin Davis <davisdustin328@gmail.com>
2024-01-31 14:09:58 -0600

    Adding function

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -14,4 +14,11 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc",),
     ])
     @patch('client.get_json')
-    
+    def test_org(self, org_name, mock_get_json):
+        """Testing GithubOrgClient.org returns the correct value."""
+        mock_get_json.return_value = {"name": org_name}
+        github_org_client = GithubOrgClient(org_name)
+        expected_result = {"name": org_name}
+
+        self.assertEqual(github_org_client.org, expected_result)
+        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
