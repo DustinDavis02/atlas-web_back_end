a982c72f6590bbc02f883db0dd31e8e34110e4c4
Dustin Davis <davisdustin328@gmail.com>
2024-01-31 14:07:29 -0600

    Parameterized

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Testing the GithubOrgClient class."""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    
