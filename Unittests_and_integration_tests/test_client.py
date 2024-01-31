b796aa806c805fb3356d9cef7f00723421fbcb8b
Dustin Davis <davisdustin328@gmail.com>
2024-01-31 15:19:57 -0600

    Mocking a property

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -22,3 +22,11 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(github_org_client.org, expected_result)
         mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
+
+    def test_public_repos_url(self):
+    """Testing property"""
+    json_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+    with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+        mock_org.return_value = json_payload
+        github_org_client = GithubOrgClient("google")
+        self.assertEqual(github_org_client._public_repos_url, json_payload["repos_url"])
