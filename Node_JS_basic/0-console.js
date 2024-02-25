17b0ab8d212758f4ed9f425aa16cbe661cfd1523
Dustin Davis <davisdustin328@gmail.com>
2024-02-25 15:24:29 -0600

    Executing basic javascript with Node JS

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// Define the function
+function displayMessage(message) {
+    console.log(message);
+  }
+  
+  // Export the function to make available for import
+  module.exports = displayMessage;
