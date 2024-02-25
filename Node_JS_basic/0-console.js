98c94c26054ee4eb47c21ddfe3da6ee33aaabdf8
Dustin Davis <davisdustin328@gmail.com>
2024-02-25 16:13:24 -0600

    update

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // Define the function
 function displayMessage(message) {
-    console.log(message);
-  }
-  
-  module.exports = displayMessage;
+  console.log(message);
+}
+
+module.exports = displayMessage;
