36d728191aaf3a108add877b7d10f63d40b97bb6
Dustin Davis <davisdustin328@gmail.com>
2024-02-28 16:26:58 -0600

    fix again

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -26,7 +26,7 @@ function countStudents(path) {
     Object.entries(fields).forEach(([field, { count, names }]) => {
       console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
     });
-} catch (err) {
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
