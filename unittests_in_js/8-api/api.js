3cfca22fc10be9e32211bfa7359d44d3e55c105e
Dustin Davis <davisdustin328@gmail.com>
2024-02-28 17:27:04 -0600

    Update API

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-const server = app.listen(7865, () => {
+app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
 
