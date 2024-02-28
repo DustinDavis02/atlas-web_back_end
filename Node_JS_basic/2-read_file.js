340708637a56a617ab634e44c9e71bf86ab68cfe
Dustin Davis <davisdustin328@gmail.com>
2024-02-28 15:50:24 -0600

    Reading a file synchronously with Node JS

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, { encoding: 'utf8' });
+
+    const lines = data.split('\n').filter(Boolean);
+
+    const students = lines.slice(1);
+
+    const fields = {};
+
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+
+      if (!fields[field]) {
+        fields[field] = { count: 0, names: [] };
+      }
+
+      fields[field].count += 1;
+      fields[field].names.push(firstname);
+    });
+
+    console.log(`Number of students: ${students.length}`);
+
+    Object.entries(fields).forEach(([field, { count, names }]) => {
+      console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
+    });
+
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
