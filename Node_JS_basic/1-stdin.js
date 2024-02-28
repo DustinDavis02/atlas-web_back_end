b583b2197bb40ab642a809af68d4ce15b9468805
Dustin Davis <davisdustin328@gmail.com>
2024-02-28 13:33:21 -0600

    Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+
+  console.log(`Your name is: ${name}`);
+
+  process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
