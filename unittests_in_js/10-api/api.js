2a39455b3ae71782af5ea57c0b616ccc4c6cacb6
Dustin Davis <davisdustin328@gmail.com>
2024-02-28 19:15:02 -0600

    New Endpoint

---
+++ unittests_in_js/10-api/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  const paymentMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  };
+  res.json(paymentMethods);
+});
+
+app.post('/login', (req, res) => {
+  const userName = req.body.userName;
+  res.send(`Welcome ${userName}`);
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+})
+
+module.exports = app;
